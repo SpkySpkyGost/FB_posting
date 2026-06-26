dc7eb972716ff5b469992bfee609401af085e1d5
SpkySpkyGost <sashasand911@gmail.com>
2026-06-26 22:59:03 +0200

    Collecting static files fix

--- build.sh
+++ build.sh
@@ -9,4 +9,7 @@ python3 -m pip install -r requirements.txt --break-system-packages
 echo "Running database migrations..."
 python3 manage.py migrate --noinput
 
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
+
 echo "=== BUILD END ==="
