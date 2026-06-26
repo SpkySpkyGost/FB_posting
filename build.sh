6c7a5e5a1c04df5f4acd9c51e83c60a386d796a1
SpkySpkyGost <sashasand911@gmail.com>
2026-06-26 21:46:58 +0200

    Improved build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 
-echo "Installing dependencies with uv..."
-uv pip install --system -r requirements.txt
+echo "Installing dependencies..."
+uv pip install --system -r requirements.txt || echo "pip install  failed or no changes"
 
 echo "Running database migrations..."
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput || echo "Migrate failed or no changes"
 
 echo "Build completed successfully!"
